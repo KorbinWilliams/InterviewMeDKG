ade25a846ae7986ad65cbc999ed59123792186c1
GabeMcClaran <gabemcclaran@gmail.com>
2020-01-15 15:49:56 -0700

    deleted rogue s

--- server/Socket/SocketService.js
+++ server/Socket/SocketService.js
@@ -1,18 +1,16 @@
-class Socket{
-    socket(io){
-        this.io=io
-        this.rooms={}
-        //Server listeners
-        io.on("connection", socket => this.newConnection(socket));
-        io.on("join", data => this.joinRoom(data))
-            }
-        newConnection(socket){
-            //Handshake/ Confirm connection
-            socket.emit("CONNECTED", {
-                socket: socket.id,
-                message: "Successfully Connected"
-            });
-        }
-        }
-
+class Socket {
+  socket(io) {
+    this.io = io;
+    this.rooms = {};
+    //Server listeners
+    io.on("connection", socket => this.newConnection(socket));
+    io.on("join", data => this.joinRoom(data));
+  }
+  newConnection(socket) {
+    //Handshake/ Confirm connection
+    socket.emit("CONNECTED", {
+      socket: socket.id,
+      message: "Successfully Connected"
+    });
+  }
 }
