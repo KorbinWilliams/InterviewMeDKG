40c9f5dd306edcd2f786163f18b58fec7ee8b89d
GabeMcClaran <gabemcclaran@gmail.com>
2020-01-16 13:59:25 -0700

    homepage set-ish and router links work

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,31 +1,42 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 import View_Home from "../views/View_Home";
 import View_Login from "../views/View_Login";
 import View_Create_Account from "../views/View_Create_Account";
-
-Vue.use(Router)
+import View_Quiz_Categories from "../views/View_Quiz_Categories";
+import View_TextChat_Categories from "../views/View_TextChat_Categories";
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: View_Home
     },
     {
-      path: '/sign_up',
-      name: 'sign_up',
+      path: "/sign_up",
+      name: "sign_up",
       component: View_Create_Account
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: View_Login
     },
     {
+      path: "/quiz_categories",
+      name: "quiz_categories",
+      component: View_Quiz_Categories
+    },
+    {
+      path: "/text_categories",
+      name: "text_categories",
+      component: View_TextChat_Categories
+    },
+    {
       path: "*",
-      redirect: '/'
+      redirect: "/"
     }
   ]
-})
+});
---
+++ client/src/views/View_Quiz_Categories.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="categories">
+    <div class="card-deck">
+      <P>categories</P>
+      <categories
+        v-for="category in categories"
+        :key="category.id"
+        :category="category"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "quiz_categories",
+  data() {
+    return {
+      name: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+#categories {
+  background: rgb(159, 159, 248);
+}
+</style>
