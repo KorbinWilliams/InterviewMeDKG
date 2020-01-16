62ae5a6dcdc487ed28a34c209170edfe3a245fc3
GabeMcClaran <gabemcclaran@gmail.com>
2020-01-16 10:41:14 -0700

    homepage cards

--- client/src/views/View_Home.vue
+++ client/src/views/View_Home.vue
@@ -1,21 +1,66 @@
 <template>
-	<div id="home">
-		<Widget_User/>
-	
-	</div>
+  <div id="home">
+    <Widget_User />
+    <div class="container mt-5">
+      <div class="row align-items-center mt-5" id="homeCards">
+        <div class="col-4">
+          <div class="card-block">
+            <h5 class="VSIP text-muted">Coming Soon</h5>
+            <img
+              src="https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              class="card-img"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card-block">
+            <h5>Text Interview</h5>
+            <img
+              src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+              class="card-img"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card-block">
+            <h5>Quiz</h5>
+            <img
+              src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+              class="card-img"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	import Widget_User from "../components/Widget_User";
-	
-	export default {
-		name: "View_Home"
-		, components: {
-			Widget_User
-		}
-	}
+import Widget_User from "../components/Widget_User";
+
+export default {
+  name: "View_Home",
+  components: {
+    Widget_User
+  }
+};
 </script>
 
 <style scoped>
-
+#homeCards {
+  /* display: flex; */
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: center;
+}
+.container {
+  height: 100vh;
+}
+.card-block {
+  padding: 10px 10px 10px 10px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+}
 </style>
