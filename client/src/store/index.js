b617b9565cb3a744c0e0ea19b841071bbf6947a9
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-01-15 17:02:51 -0700

    edited state and quiz

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,59 +4,61 @@ import Axios from 'axios';
 import router from '../router/index';
 import AuthService from '../AuthService';
 
-Vue.use (Vuex);
+Vue.use(Vuex);
 
-let base = window.location.host.includes ('localhost:8080') ? '//localhost:3000/' : '/';
+let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/';
 
-let api = Axios.create ({
+let api = Axios.create({
 	baseURL: base + "api/",
 	timeout: 3000,
 	withCredentials: true
 })
 
-export default new Vuex.Store ({
+export default new Vuex.Store({
 	state: {
 		user: {},
-		boards: [],
-		activeBoard: {}
+		userProfile: {},
+		activeProfile: {},
+		categories: [],
+		activeQuiz: {}
 	},
 	mutations: {
-		setUser (state, user) {
+		setUser(state, user) {
 			state.user = user
 		},
 	},
 	actions: {
 		//#region -- AUTH STUFF --
-		async register ({commit, dispatch}, creds) {
+		async register({ commit, dispatch }, creds) {
 			try {
-				let user = await AuthService.Register (creds);
-				commit ('setUser', user);
-				router.push ({name: "boards"})
+				let user = await AuthService.Register(creds);
+				commit('setUser', user);
+				router.push({ name: "boards" })
 			} catch (e) {
-				console.warn (e.message)
+				console.warn(e.message)
 			}
 		},
-		async login ({commit, dispatch}, creds) {
+		async login({ commit, dispatch }, creds) {
 			try {
-				let user = await AuthService.Login (creds);
-				commit ('setUser', user);
-				router.push ({name: "boards"})
+				let user = await AuthService.Login(creds);
+				commit('setUser', user);
+				router.push({ name: "boards" })
 			} catch (e) {
-				console.warn (e.message)
+				console.warn(e.message)
 			}
 		},
-		async logout ({commit, dispatch}) {
+		async logout({ commit, dispatch }) {
 			try {
-				let success = await AuthService.Logout ();
+				let success = await AuthService.Logout();
 				if (!success) {
 				}
-				commit ('resetState');
-				router.push ({name: "login"});
+				commit('resetState');
+				router.push({ name: "login" });
 			} catch (e) {
-				console.warn (e.message)
+				console.warn(e.message)
 			}
 		},
 		//#endregion
-		
+
 	}
 })
---
+++ client/src/views/Quiz.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="quiz">
+    <h1>
+      <!-- quiz title -->
+    </h1>
+    <div class="question" v-for="question in questions" :key="question._id">
+      <!-- v-for on questions v-show or v-if next on completion or pressing next question
+      show answer for previous question on completion-->
+      <!-- v-if quiz.multiple = true show different component for question or switch between form input and button input -->
+    </div>
+    <div class="answer">
+      <h3 v-for="answer in answers" :key="answer.id"></h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz",
+  data() {
+    answer: "";
+  },
+  mounted() {
+    this.$store.dispatch("getActiveQuiz");
+  },
+  methods: {},
+  computed: {
+    questions() {
+      this.$store.state.activeQuiz.questions;
+    }
+  },
+  components: {}
+};
+</script>
+
+<style>
+</style>
